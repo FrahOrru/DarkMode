734a5efa953fbdc5a9ca8282efdfbd33af6c9d41
FrahOrru <francesca.orru@marconirovereto.it>
2020-09-20 14:55:35 +0200

    switch

--- Script.js
+++ Script.js
@@ -1,18 +1,3 @@
-function DarkMode(){
- var bod = document.getElementById('card').style.backgroundColor = "#031220";
- document.getElementById('card').style.color = "white";
-
-//  document.getElementById('btn').style.display = "none";
-
-// document.getElementById('card').innerHTML =
-// `<img src="#" alt="Avatar" style="width:100%">
-// <div class="container">
-// <h4><b>John Doe</b></h4> 
-// <p>Architect & Engineer</p> 
-// </div>
-// <button id="btnW">WHITE MODE</button>`
- 
-
-document.getElementById('btn').style.backgroundColor = "#031220";
-document.getElementById('btn').style.color = "white";
-}
+$(".theme-switch").on("click", () => {
+    $("body").toggleClass("light-theme");
+  });
